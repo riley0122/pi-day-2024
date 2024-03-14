4ee8b8554452fd9a2238bfb1a33bb55a15987744
Bread <77288737+riley0122@users.noreply.github.com>
2024-03-14 18:17:15 +0100

    Added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,8 @@ export const metadata: Metadata = {
   description: "Memorize as many digits as pi and see how much you can remember!",
 };
 
+import { Analytics } from "@vercel/analytics/react"
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,6 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* TODO switch over to google analytics via cloudflare zaraz */}
+        <Analytics />
+      </head>
       <body className={inter.className}>
         {children}
         <a href="https://github.com/riley0122/pi-day-2024" className="text-gray-600 decoration-dotted underline absolute bottom-5 right-1/2 translate-x-1/2">
